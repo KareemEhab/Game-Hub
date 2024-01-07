a59ae0281fa50726c80c24ffb13415149af67f29
KareemEhab <kareem8002010@gmail.com>
2024-01-07 16:58:22 +0200

    Add emojix

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -12,7 +12,7 @@ interface Props {
 
 const GameGrid = ({ gameQuery }: Props) => {
   const { data: games, error, isLoading } = useGames(gameQuery);
-  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
   return (
     <>
       {error && <Text>{error}</Text>}
